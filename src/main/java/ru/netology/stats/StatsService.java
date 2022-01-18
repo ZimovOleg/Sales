766c30ffd285f6709776857a8bcf157ad546aa1a
ZimovOleg <zimov.oleg@gmail.com>
2022-01-18 19:47:01 +0400

    src/main/java/ru/netology/stats/StatsService.java

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,62 +2,94 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public int countSumOfSalesAllMonths(int[]salesOnMonths){
+    public int totalSalesForAllMonths  (int[]salesOnMonths){
+        // Метод Сумма прибыли по всем месяцам
         int sum = 0;
+        // задаем переменную "сумма" , равную 0
         for (int sale : salesOnMonths) {
+            // берем Sale значение переменной из массива
             sum += sale;
+            // прибавляем значение Sale к Sum
         }
         return sum;
     }
 
-    public int countMedian(int[]salesOnMonths){
-        int median = countSumOfSalesAllMonths(salesOnMonths) / salesOnMonths.length;
-        return median;
+    public int averageProfitMonths(int[]salesOnMonths){
+        // Метод средняя прибыль
+        // задаем переменную "Average" равную делению переменной сумма с кол-вом
+        // значений в массиве
+        int Average = totalSalesForAllMonths (salesOnMonths) / salesOnMonths.length;
+        return Average;
 }
 
-    public int monthMaxSales(int[]salesOnMonths){
-        int currentMax = salesOnMonths[0];
-        int numberOfMonthMaxSales = 0;
-        for (int i = 0; i < salesOnMonths.length; i++) {
-            if (salesOnMonths [i] >= currentMax) {
-                currentMax = salesOnMonths[i];
-                numberOfMonthMaxSales = i + 1;
-            }
-        }
-        return numberOfMonthMaxSales;
-    }
-
-    public int monthMinSales(int[]salesOnMonths){
-        int currentMin = salesOnMonths[0];
-        int numberOfMonthMinSales = 0;
-        for (int i = 0; i < salesOnMonths.length; i++) {
-            if (salesOnMonths [i] <= currentMin) {
-                currentMin = salesOnMonths[i];
-                numberOfMonthMinSales = i + 1;
-            }
-        }
-        return numberOfMonthMinSales;
-    }
-
-    public int monthsSalesBelowMedian(int[]salesOnMonths){
-        int medianSales = countMedian(salesOnMonths);
-        int monthsBelowMedian = 0;
+    public int monthMaxSales (int[] salesOnMonths) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
         for (int sale : salesOnMonths) {
-            if (sale < medianSales) {
-                monthsBelowMedian++;
+            if (sale >= salesOnMonths[maxMonth]) {
+                maxMonth = month;
             }
+            month = month +1; // следующий рассматриваемый месяц имеет номер на 1 больше
         }
-        return monthsBelowMedian;
+        return maxMonth + 1;
+    }
+//public int monthMaxSales(int[]salesOnMonths){
+//        // Метод расчета месяца, в котором был пик продаж
+//        // задаем переменную с точкой отсчета первый элемент массива
+//        int monthMax = salesOnMonths[0];
+//        // задаем "кол-во макс. месяцев" переменную со значением ноль
+//        int numberOfMonthMaxSales = 0;
+//        for (int month = 0; month < salesOnMonths.length; month++) {
+//            // месяц = о, месяц меньше кол-ва значений в массиве, месяц +1
+//            if (salesOnMonths [month] >= monthMax) {
+//                //
+//                monthMax = salesOnMonths[month];
+//                numberOfMonthMaxSales = month + 1;
+//            }
+//        }
+//        return numberOfMonthMaxSales;
+//    }
+    public int monthMinSales (int[] salesOnMonths) {
+    int minMonth = 0;
+    int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+    for (int sale : salesOnMonths) {
+         if (sale <= salesOnMonths[minMonth]) {
+             minMonth = month;
+        }
+    month = month +1; // следующий рассматриваемый месяц имеет номер на 1 больше
+    }
+     return minMonth + 1;
     }
 
-    public int monthsSalesOverMedian(int[]salesOnMonths){
-        int medianSales = countMedian(salesOnMonths);
-        int monthsOverMedian = 0;
+    public int monthsSaleslowAverage (int[]salesOnMonths){
+        // Метод расчета кол-во месяцев, с продажами были ниже среднего
+        int monthsSales = averageProfitMonths(salesOnMonths);
+        // задаем переменную равную средней прибыли
+        int monthsBelowAverage = 0;
+        // задаем переменную кол-во месяцев равную 0
         for (int sale : salesOnMonths) {
-            if (sale > medianSales) {
-                monthsOverMedian++;
+            // переменная Sale берет значение из массива salesOnMonths
+            if (sale < monthsSales) {
+                // если sale меньше, прибавляем к кол-ву месяц +1
+                monthsBelowAverage++;
             }
         }
-        return monthsOverMedian;
+        return monthsBelowAverage;
+    }
+
+    public int monthsSalesMaxAverage(int[]salesOnMonths){
+        // Метод расчета кол-во месяцев, с продажами были выше среднего
+        int monthsSales = averageProfitMonths(salesOnMonths);
+        // задаем переменную равную средней прибыли
+        int monthsMaxMedian = 0;
+        // задаем переменную кол-во месяцев равную 0
+        for (int sale : salesOnMonths) {
+           // переменная Sale берет значение из массива salesOnMonths
+            if (sale > monthsSales) {
+                // если Sale больше средней прибыли добавляем +1
+                monthsMaxMedian++;
+            }
+        }
+        return monthsMaxMedian;
     }
 }
